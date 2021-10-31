57b1c145fbae1fbb543b6d555b3526b155c6a5d5
Olivia Park <olivia@bioteam.net>
2021-10-30 23:50:10 -0500

    Adding in cartoon style support

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import MolRenderer from './MolRenderer';
-import model from './sampleData/7m53'
+import MolRenderer, { RENDER_STYLES } from './MolRenderer';
+import pdb from './sampleData/7m53'
+import asprin from './sampleData/asprin'
 
 const appStyles = {
   display: 'flex', 
@@ -10,8 +11,8 @@ const appStyles = {
 }
 
 function App() {
-  const [viewStyle, setViewStyle] = useState({stick:{}})
-
+  const [viewStyle, setViewStyle] = useState(RENDER_STYLES.Stick)
+  const [model, setModel] = useState(pdb)
   return (
     <div className="App" style={appStyles}>
       <h1>React 3D Mol Demo</h1>
@@ -22,12 +23,18 @@ function App() {
         fileType={model.type}
         style={viewStyle}
       />
+      <h3>Molecules</h3>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+        <button style={{margin: '10px'}} onClick={() => {setModel(asprin); setViewStyle(RENDER_STYLES.Stick)}}>Asprin</button>
+        <button style={{margin: '10px'}} onClick={() => {setModel(pdb)}}>7m53</button>
+      </div>
       <h3>View Styles</h3>
       <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-        <button style={{margin: '10px'}} onClick={() => {setViewStyle({stick:{}})}}>Stick</button>
-        <button style={{margin: '10px'}} onClick={() => {setViewStyle({line:{}})}}>Line</button>
-        <button style={{margin: '10px'}} onClick={() => {setViewStyle({cross:{linewidth:5}})}}>Cross</button>
-        <button style={{margin: '10px'}} onClick={() => {setViewStyle({sphere:{}})}}>Sphere</button>
+        <button style={{margin: '10px'}} onClick={() => {setViewStyle(RENDER_STYLES.Stick)}}>Stick</button>
+        <button style={{margin: '10px'}} onClick={() => {setViewStyle(RENDER_STYLES.Line)}}>Line</button>
+        <button style={{margin: '10px'}} onClick={() => {setViewStyle(RENDER_STYLES.Cross)}}>Cross</button>
+        <button style={{margin: '10px'}} onClick={() => {setViewStyle(RENDER_STYLES.Sphere)}}>Sphere</button>
+        <button style={{margin: '10px'}} disabled={model.type === "sdf"} onClick={() => {setViewStyle(RENDER_STYLES.Cartoon)}}>Cartoon</button>
       </div>
     </div>
   );
--- src/MolRenderer.jsx
+++ src/MolRenderer.jsx
@@ -4,6 +4,14 @@ import React, { useEffect, useState } from 'react';
 window.$ = jQuery;
 const $3Dmol = require('3dmol');
 
+export const RENDER_STYLES = {
+  Stick: {stick:{colorscheme:'Jmol'}},
+  Line: {line:{colorscheme:'Jmol'}},
+  Cross: {cross:{lineWidth:5, colorscheme:'Jmol'}},
+  Sphere: {sphere:{colorscheme:'Jmol'}},
+  Cartoon: {cartoon:{}}
+}
+
 const MolRenderer = (props) => {
   const [container, setContainer] = useState()
   const [viewer, setViewer] = useState()
@@ -11,19 +19,27 @@ const MolRenderer = (props) => {
   useEffect(() => {
     if(container) {
       const glViewer = $3Dmol.createViewer(jQuery(container), {
-        defaultcolors: $3Dmol.elementColors.rasmol,
+        defaultcolors: $3Dmol.elementColors.Jmol,
       });
       glViewer.addModel(props.modelData, props.fileType)
       glViewer.setBackgroundColor("#eeeeee")
       glViewer.zoomTo();
       glViewer.render();
-      glViewer.zoom(1.5, 1000);
+      glViewer.zoom(1, 1000);
       setViewer(glViewer)
     }
   }, [container, props.modelData, props.fileType])
 
   useEffect(() => {
     if(viewer) {
+      let model = viewer.getModel()
+      if(props.style === RENDER_STYLES.Cartoon) {
+        model.setColorByFunction({}, (atom) => {
+          if(atom.ss == 'h') return "magenta";
+          else if(atom.ss == 's') return "orange";
+          else return "white";
+        });
+      }
       viewer.setStyle({}, props.style)
       viewer.render();
     }
